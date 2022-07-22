2c8434fde2df1621075ab6b3488ae6c9b6e33648
Slav <itiksolution@gmail.com>
2022-07-22 14:15:58 +1000

    assignment complete

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,6 +2,7 @@ const chai = require('chai')
 const expect = chai.expect;
 
 describe('index.js', function() {
+  
   describe('multiply', function() {
     it("is an equation whose multiplied results will equal 62", function() {
       expect(num1).to.be.a('number')
